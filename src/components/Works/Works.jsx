9ed0b53bb097a8a9a574e20a71deaec0aa27a9c5
yulianiia_sierykh <julis098765@gmail.com>
2021-12-14 19:27:13 +0200

    fixed links target

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -21,14 +21,14 @@ export default function Works() {
           <img src={dashboard} alt="diving landing" width="500" />
           <div className="description">
             <p><b>Kanban board</b><br/>
-              Used <a href="https://board-apiapp.herokuapp.com/api/boards" style={{ color: 'rgba(0, 0, 0, 0.6)' }}>board-apiapp.herokuapp.com</a> API for the backend<br />
+              Used <a href="https://board-apiapp.herokuapp.com/api/boards" target="_blank" style={{ color: 'rgba(0, 0, 0, 0.6)' }}>board-apiapp.herokuapp.com</a> API for the backend<br />
               Registration and authentication implemented<br /> Drag and drop events used<br />
               Written with react, redax, react hooks
             </p>
 
             <div className="buttons_container">
               <button type="button">
-                <a href="https://peaceful-almeida-c7958e.netlify.app">TRY IT YOURSELF</a>
+                <a href="https://peaceful-almeida-c7958e.netlify.app" target="_blank">TRY IT YOURSELF</a>
               </button>
               <button onClick={openModalHendler} type="button">DEMO VIDEO</button>
             </div>
@@ -39,13 +39,13 @@ export default function Works() {
           <img src={movies} alt="movies landing" width="500" />
           <div className="description">
             <p><b>Movies service</b><br/>
-                Used <a href="https://www.themoviedb.org/" style={{color: 'rgba(0, 0, 0, 0.6)'}}>themoviedb.org</a> API for the backend<br/>
+                Used <a href="https://www.themoviedb.org/" target="_blank" style={{color: 'rgba(0, 0, 0, 0.6)'}}>themoviedb.org</a> API for the backend<br/>
                 Written with react, redax
             </p>
 
             <div className="buttons_container">
               <button type="button">
-                <a href="https://angry-mestorf-bfac13.netlify.app">TRY IT YOURSELF</a>
+                <a href="https://angry-mestorf-bfac13.netlify.app" target="_blank">TRY IT YOURSELF</a>
               </button>
             </div>
           </div>
@@ -61,7 +61,7 @@ export default function Works() {
 
             <div className="buttons_container">
               <button type="button">
-                <a href="https://naughty-hermann-3246c3.netlify.app">TRY IT YOURSELF</a>
+                <a href="https://naughty-hermann-3246c3.netlify.app" target="_blank">TRY IT YOURSELF</a>
               </button>
             </div>
           </div>
